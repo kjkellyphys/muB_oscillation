fixed average over dPdX from Kevins magic formulae

MicroTools/InclusiveTools/inclusive_osc_tools.py
@@ -498,6 +498,7 @@ def Decay_muB_OscChi2(
     decouple_decay=False,
     disappearance=True,
     energy_degradation=True,
+    helicity="conserving",
 ):
     """Calculates the chi-squared from the full covariance matrix,
     allowing for oscillated backgrounds (oscillating as a function of *reconstructed* neutrino energy)
@@ -531,12 +532,19 @@ def Decay_muB_OscChi2(
     energy_degradation: bool, optional
         whether to include energy degradation in disappearance channel, by default True.
         If False, return to usual disappearance probability
+
+    helicity: str, optional
+        whether to include conserving or flipping helicity, by default "conserving".
     """
     CVStat = np.zeros(np.shape(FCov))
     CVSyst = np.zeros(np.shape(FCov))
     # Load the Sterile class from param_scan
     sterile = Sterile(
-        theta, oscillations=oscillations, decay=decay, decouple_decay=decouple_decay
+        theta,
+        oscillations=oscillations,
+        decay=decay,
+        decouple_decay=decouple_decay,
+        helicity=helicity,
     )
     if sigReps is not None:
         if len(sigReps) != 7:
@@ -682,12 +690,17 @@ def DecayMuBNuEDis(
     decouple_decay=False,
     disappearance=True,
     energy_degradation=True,
+    helicity="conserving",
 ):
     """Function for reweighting MicroBooNE nu_e spectra in terms of true energy instead of reconstructed energy"""
 
     # Load the Sterile class from param_scan
     sterile = Sterile(
-        theta, oscillations=oscillations, decay=decay, decouple_decay=decouple_decay
+        theta,
+        oscillations=oscillations,
+        decay=decay,
+        decouple_decay=decouple_decay,
+        helicity=helicity,
     )
     PeeRW = []
     # MCT is MiniBooNE truth level distribution from 2018. That's why it needs to be rescaled when unfolding
@@ -732,12 +745,17 @@ def DecayMuBNuMuDis(
     decouple_decay=False,
     disappearance=True,
     energy_degradation=True,
+    helicity="conserving",
 ):
     """Function for reweighting MicroBooNE nu_mu spectra in terms of true energy instead of reconstructed energy"""
 
     # Load the Sterile class from param_scan
     sterile = Sterile(
-        theta, oscillations=oscillations, decay=decay, decouple_decay=decouple_decay
+        theta,
+        oscillations=oscillations,
+        decay=decay,
+        decouple_decay=decouple_decay,
+        helicity=helicity,
     )
     PmmRW_FC = []
     PmmRW_PC = []

MicroTools/plot_tools.py
@@ -736,7 +736,9 @@ def make_numu_rate_plot(rates, params, name="3+1_osc"):
 MuBchi2_null_hyp = 93
 
 
-def make_micro_rate_plot(rates, params, name="micro_3+1_osc", PC=False):
+def make_micro_rate_plot(
+    rates, params, name="micro_3+1_osc", PC=False, helicity="conserving"
+):
     fig, ax1 = std_fig(figsize=(3.3 * 1.2, 2 * 1.2))
 
     bins = np.array([0.0 + 0.1 * j for j in range(26)] + [10.0])
@@ -783,6 +785,7 @@ def make_micro_rate_plot(rates, params, name="micro_3+1_osc", PC=False):
         decouple_decay=False,
         disappearance=True,
         energy_degradation=True,
+        helicity=helicity,
     )
 
     # \nu_mu disappearance signal replacement
@@ -794,6 +797,7 @@ def make_micro_rate_plot(rates, params, name="micro_3+1_osc", PC=False):
         decouple_decay=False,
         disappearance=True,
         energy_degradation=True,
+        helicity=helicity,
     )
 
     # MicroBooNE
@@ -808,6 +812,7 @@ def make_micro_rate_plot(rates, params, name="micro_3+1_osc", PC=False):
         decouple_decay=False,
         disappearance=True,
         energy_degradation=True,
+        helicity=helicity,
     )
 
     ######################################

MicroTools/sterile_tools.py
@@ -100,9 +100,11 @@ class Sterile:
         self.CP = CP
         self.helicity = helicity
         if self.helicity == "conserving":
-            self.dPdecaydX = self.dPdecaydX_conserving
+            self.dPdecaydX = Sterile.dPdecaydX_conserving
+            self.dPdecaydX_Avg = Sterile.dPdecaydX_Avg_conserving
         elif self.helicity == "flipping":
-            self.dPdecaydX = self.dPdecaydX_flipping
+            self.dPdecaydX = Sterile.dPdecaydX_flipping
+            self.dPdecaydX_Avg = Sterile.dPdecaydX_Avg_flipping
         else:
             raise ValueError('helicity must be "conserving" or "flipping"')
 
@@ -367,7 +369,7 @@ class Sterile:
         pdecay = (
             self.Um4Sq
             * self.FdecAvg_analytical(Emin, Emax, Length)
-            * (1 - ((Eintmax**2 - Eintmin**2) / ((Emax - E0) * (Emax + E0))))
+            * self.dPdecaydX_Avg(E0, Emin, Emax, Eintmin, Eintmax)
             * DegradationCorrection(
                 (Eintmin + Eintmax) / 2,
                 (Emin + Emax) / 2,
@@ -400,7 +402,7 @@ class Sterile:
         pdecay = (
             self.Ue4Sq
             * self.FdecAvg_analytical(Emin, Emax, Length)
-            * (1 - ((Eintmax**2 - Eintmin**2) / ((Emax - E0) * (Emax + E0))))
+            * self.dPdecaydX_Avg(E0, Emin, Emax, Eintmin, Eintmax)
             * DegradationCorrection(
                 (Eintmin + Eintmax) / 2,
                 (Emin + Emax) / 2,
@@ -408,7 +410,6 @@ class Sterile:
                 noffset=noffset,
             )
         )
-        # ((Eintmax**2 - Eintmin**2)/(Emax*Emin)) factor is to account for the decay rate scaling with Eint/E4
         # It gives the fraction of events in this bin
         if not self.decouple_decay:
             # overlap of daughter with nu_e state
@@ -483,6 +484,27 @@ class Sterile:
         """
         return 2 * (Edaughter / Eparent)
 
+    @staticmethod
+    @numba.jit(nopython=True, cache=True)
+    def dPdecaydX_Avg_conserving(E0, Emin, Emax, Eintmin, Eintmax):
+        if Emin == E0 or Emax == E0:
+            return 1
+        else:
+            return (Eintmax**2 - Eintmin**2) / ((Emax - E0) * (Emin + E0))
+
+    @staticmethod
+    @numba.jit(nopython=True, cache=True)
+    def dPdecaydX_Avg_flipping(E0, Emin, Emax, Eintmin, Eintmax):
+        if Emin == E0 or Emax == E0:
+            return 1
+        else:
+            return (
+                ((Emax + Emin) - (Eintmax + Eintmin))
+                * (Eintmax - Eintmin)
+                / (Emax - E0)
+                / (Emin - E0)
+            )
+
     def EnergyDegradation(
         self, R_in_Enutrue, Etrue_bins_edge, which_channel, which_experiment
     ):
@@ -510,7 +532,7 @@ class Sterile:
 
                 R_deg[k][i] = (
                     Pdecay * R_in_Enutrue[k]
-                )  # k indexes true energy, i indexes degraded energy
+                )  # k indexes parent energy, i indexes daughter energy
 
         R_sum = np.sum(R_deg, axis=0)
 
@@ -601,7 +623,6 @@ f_sigma = np.load(
 
 def Xsec(E):
     """Cross section in cm^2, E -- GeV"""
-    # polyfit = -129.16 * E**4 + 146.38 * E**3 - 40.046 * E**2 + 4.5652 * E + 0.0358
     return f_sigma(E)
 
 
@@ -611,10 +632,10 @@ def DegradationCorrection(Edaughter, E4, exp, noffset=0):
         return Xsec(Edaughter) / Xsec(E4) * MiniEff(Edaughter) / MiniEff(E4)
     elif exp == "microboone":
 
-        if Edaughter < 1:  # Should this be Emax or Edaughtermax? We should discuss.
-            n = 2 + noffset
-        else:
+        if Edaughter < 1:
             n = 1 + noffset
+        else:
+            n = 0 + noffset
 
         xsec_nu4 = Xsec(E4)
         return (